e901c6992fbb58cadf0058f71eb418d6fb8f4d1f
Andreas Røed <andreas@kommunal-rapport.no>
2016-05-02 12:35:39 +0200

    Switching between multiple data layers is now possible.

--- themes/bartik/js/init_carto.js
+++ themes/bartik/js/init_carto.js
@@ -1,60 +1,75 @@
 //Global Map Object
 var map;
-var layerUrl = 'https://andreasroeed.cartodb.com/api/v2/viz/042a9344-0dd7-11e6-8421-0e3ff518bd15/viz.json';
+var layerUrl = "https://andreasroeed.cartodb.com/api/v2/viz/a499d7c2-0dd7-11e6-83b7-0ea31932ec1d/viz.json";
+var sublayers = [];
+
+var cssyes = "#n2000_kommunekart_uten_hav_data{line-color: #000000;polygon-fill:#008000}";
+var cssno = "#n2000_kommunekart_uten_hav_data{line-color: #000000;polygon-fill:#ff0000}";
 
 function main() {
-    $(document).ready(function(){
+    
+$(document).ready(function(){
 
-      map = cartodb.createVis('map', layerUrl,{
-           shareable: false,
-           title: false,
-           search: false, 
-           tiles_loader: true,
-               center: [59,10],
-           zoom: 5
-      }).done(function(vis, layers) {
-        console.log(vis);
-        console.log(layers);
-      });
-      
-    });
-
-}//main ends
-
-function addLayer(){
-  var map = new L.Map('map', {
-    center: [59,10],
-    zoom: 5
-  });     
-
-
-  var subLayerOptions = {
-    sql: "SELECT * FROM n2000_kommunekart_uten_hav_data WHERE JA >= 8000",
-    cartocss: "#n2000_kommunekart_uten_hav_data{line-color: #44ffdd}"
+  // Choose center and zoom level
+  var options = {
+    center: [63.44,10.42], // Trondheim
+    zoom: 4
   }
 
+  // Instantiate map on specified DOM element
+  map = new L.Map('map',options);
 
-  var sublayers = [];
+  // Add a basemap to the map object just created
+  L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+    attribution: 'Stamen'
 
-      cartodb.createLayer(map,layerUrl).addTo(map).on("done",function(layer){
+  }).addTo(map);
 
+
+
+  cartodb.createLayer(map,layerUrl).addTo(map).on("done",function(layer){
         var sublayer = layer.getSubLayer(0);
-        sublayer.set(subLayerOptions);
-        sublayer.setInteractive(false);
         sublayers.push(sublayer);
-        console.log(sublayers);
 
       }).on("error",function(err){
           alert("err: "+err);
       });
+      
+  });
+
+}//main ends
 
 
-}//addLayer ends
 
 window.onload = main;
+
+
+
+
+var layerSelections = {
+  No: function(){
+   sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE NEI > JA");
+   sublayers[0].setCartoCSS(cssno);
+   return true;
+  },
+  Yes: function(){
+   sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE JA > NEI");
+   sublayers[0].setCartoCSS(cssyes);
+  }
+
+}
+
+
+
+//Handle click events
 $(document).ready(function(){
-  $("#makeQuery").click(function(){
-  addLayer();
-});
+
+  $("#No").click(function(){
+    layerSelections["No"]();
+  });
+
+  $("#Yes").click(function(){
+    layerSelections["Yes"]();
+  });
 
 });
