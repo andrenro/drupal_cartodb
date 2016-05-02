63441a80c2f5ebd03e38e16d9db664aec97c5083
Andreas Røed <andreas@kommunal-rapport.no>
2016-05-02 15:57:09 +0200

    Added Bootstrap CSS, added functionality for text search, max number search.

--- themes/bartik/js/init_carto.js
+++ themes/bartik/js/init_carto.js
@@ -13,7 +13,9 @@ $(document).ready(function(){
   // Choose center and zoom level
   var options = {
     center: [63.44,10.42], // Trondheim
-    zoom: 4
+    zoom: 4,
+    minZoom: 4,
+    maxZoom: 6
   }
 
   // Instantiate map on specified DOM element
@@ -22,15 +24,13 @@ $(document).ready(function(){
   // Add a basemap to the map object just created
   L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
     attribution: 'Stamen'
-
   }).addTo(map);
 
 
 
   cartodb.createLayer(map,layerUrl).addTo(map).on("done",function(layer){
-        var sublayer = layer.getSubLayer(0);
-        sublayers.push(sublayer);
-
+      var sublayer = layer.getSubLayer(0);
+      sublayers.push(sublayer);
       }).on("error",function(err){
           alert("err: "+err);
       });
@@ -43,21 +43,52 @@ $(document).ready(function(){
 
 window.onload = main;
 
-
+function getData(queryString){
+  $(document).ready(function(){
+    
+var sql = new cartodb.SQL({ user: 'andreasroeed' });
+sql.execute(queryString)
+  .done(function(data) {
+    console.log(data.rows);
+  })
+  .error(function(errors) {
+    // errors contains a list of errors
+    console.log("errors:" + errors);
+  })
+  }); 
+}
 
 
 var layerSelections = {
   No: function(){
+   let data = getData("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE NEI > JA");
+
    sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE NEI > JA");
    sublayers[0].setCartoCSS(cssno);
+   query = "SELECT count(NEI) FROM n2000_kommunekart_uten_hav_data WHERE NEI > JA";
+
    return true;
   },
   Yes: function(){
    sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE JA > NEI");
    sublayers[0].setCartoCSS(cssyes);
+   query = "SELECT count(JA) FROM n2000_kommunekart_uten_hav_data WHERE JA > NEI";
+   getData(query);
+  },
+  byCount: function(attribute, input){
+  console.log(input);
+   sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE "+attribute+" >= "+input+"");
+   sublayers[0].setCartoCSS(cssyes);
+  },
+  byName: function(attribute,input){
+   var inputString = "'%"+input+"%'";
+   var query = "SELECT * FROM n2000_kommunekart_uten_hav_data WHERE "+attribute+" ILIKE "+inputString;
+   sublayers[0].setSQL(query);
+   sublayers[0].setCartoCSS(cssyes);
   }
 
 }
+
 
 
 
@@ -66,6 +97,14 @@ $(document).ready(function(){
 
   $("#No").click(function(){
     layerSelections["No"]();
+  });
+
+  $("#byCount").change(function(){
+    layerSelections["byCount"]("NEI",$(this).val());
+  });
+
+  $("#byName").change(function(){
+    layerSelections["byName"]("navn",$(this).val());
   });
 
   $("#Yes").click(function(){
