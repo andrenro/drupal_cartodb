fd7f2702fe4e894a105af172b5afafa933e83b3d
Andreas Røed <andreas@kommunal-rapport.no>
2016-05-03 15:27:14 +0200

    Slider control added

--- themes/bartik/js/init_carto.js
+++ themes/bartik/js/init_carto.js
@@ -1,45 +1,85 @@
 //Global Map Object
 var map;
-var layerUrl = "https://andreasroeed.cartodb.com/api/v2/viz/a499d7c2-0dd7-11e6-83b7-0ea31932ec1d/viz.json";
+var layerUrl = "https://andreasroeed.cartodb.com/api/v2/viz/b88ab2b8-1108-11e6-9018-0e674067d321/viz.json";
 var sublayers = [];
 
-var cssyes = "#n2000_kommunekart_uten_hav_data{line-color: #000000;polygon-fill:#008000}";
-var cssno = "#n2000_kommunekart_uten_hav_data{line-color: #000000;polygon-fill:#ff0000}";
+var cssyes = "#n2000_m_datestamps{line-color: #000000;polygon-fill:#008000}";
+var cssno = "#n2000_m_datestamps{line-color: #000000;polygon-fill:#ff0000}";
 
 function main() {
     
 $(document).ready(function(){
 
-  // Choose center and zoom level
-  var options = {
-    center: [63.44,10.42], // Trondheim
-    zoom: 4,
-    minZoom: 4,
-    maxZoom: 6
-  }
+
+      function changeLegend(start, end) {
+          $('#legend').html("" + new Date(start).getDay()+"/"+new Date(start).getMonth()+"/"+new Date(start).getFullYear()+ " - "+new Date(end).getDay()+"/"+new Date(end).getMonth()+"/"+new Date(end).getFullYear());
+      }
+      function addTimeSlider(sublayer) {
+         var sql = cartodb.SQL({ user: 'andreasroeed' });
+         // fetch time range
+         sql.execute('SELECT max(vedtatt_dato), min(vedtatt_dato) FROM n2000_m_datestamps', function(data) {
+           var range = data.rows[0];
+           var max = new Date(range.max).getTime();
+           var min = new Date(range.min).getTime();
+
+           // update slider with range
+           $("#slider").slider({
+              min: min,
+              max: max,
+              values: [min,max],
+              slide: function(event, ui) {
+                // give feedback to the user on slide change
+                changeLegend(ui.values[0], ui.values[1]);
+              },
+              stop: function( event, ui ) { 
+                // when user selects the dates, update the layer with the range
+                // var start = new Date(ui.values[0]).toISOString()
+                // var end = new Date(ui.values[1]).toISOString();
+
+                startDate = new Date(ui.values[0]);
+                endDate = new Date(ui.values[1]);
+
+                start = startDate.getDay()+"/"+startDate.getMonth()+"/"+startDate.getFullYear()+"";
+                end = endDate.getDay()+"/"+endDate.getMonth()+"/"+endDate.getFullYear()+"";
+
+                var query = "SELECT * FROM n2000_m_datestamps WHERE vedtatt_dato >="+"'"+ start +"'"+" AND vedtatt_dato <="+"'"+ end +"'";
+                sublayers[0].setSQL(query);
+                sublayers[0].setCartoCSS(cssyes);
+              }
+            });
+            changeLegend(min, (min + max)/ 2);
+         });
+      }
+      // Choose center and zoom level
+      var options = {
+        center: [63.44,10.42], // Trondheim
+        zoom: 4,
+        minZoom: 4,
+        maxZoom: 6
+      }
 
   // Instantiate map on specified DOM element
   map = new L.Map('map',options);
-
+  var standard = 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
+  var stamen = 'http://tile.stamen.com/toner/{z}/{x}/{y}.png'
   // Add a basemap to the map object just created
-  L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+  L.tileLayer(standard, {
     attribution: 'Stamen'
   }).addTo(map);
 
-
-
-  cartodb.createLayer(map,layerUrl).addTo(map).on("done",function(layer){
+  cartodb.createLayer(map,layerUrl)
+  .addTo(map)
+  .on("done",function(layer){
       var sublayer = layer.getSubLayer(0);
+
       sublayers.push(sublayer);
-      }).on("error",function(err){
+      addTimeSlider(sublayer[0]);
+  })
+  .on("error",function(err){
           alert("err: "+err);
       });
-      
   });
-
 }//main ends
-
-
 
 window.onload = main;
 
@@ -61,28 +101,23 @@ sql.execute(queryString)
 
 var layerSelections = {
   No: function(){
-   let data = getData("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE NEI > JA");
-
-   sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE NEI > JA");
+   let data = getData("SELECT * FROM n2000_m_datestamps WHERE NEI > JA");
+   sublayers[0].setSQL("SELECT * FROM n2000_m_datestamps WHERE NEI > JA");
    sublayers[0].setCartoCSS(cssno);
-   query = "SELECT count(NEI) FROM n2000_kommunekart_uten_hav_data WHERE NEI > JA";
-
    return true;
   },
   Yes: function(){
-   sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE JA > NEI");
+   sublayers[0].setSQL("SELECT * FROM n2000_m_datestamps WHERE JA > NEI");
    sublayers[0].setCartoCSS(cssyes);
-   query = "SELECT count(JA) FROM n2000_kommunekart_uten_hav_data WHERE JA > NEI";
-   getData(query);
+   query = "SELECT count(JA) FROM n2000_m_datestamps WHERE JA > NEI";
   },
   byCount: function(attribute, input){
-  console.log(input);
-   sublayers[0].setSQL("SELECT * FROM n2000_kommunekart_uten_hav_data WHERE "+attribute+" >= "+input+"");
+   sublayers[0].setSQL("SELECT * FROM n2000_m_datestamps WHERE "+attribute+" >= "+input+"");
    sublayers[0].setCartoCSS(cssyes);
   },
   byName: function(attribute,input){
    var inputString = "'%"+input+"%'";
-   var query = "SELECT * FROM n2000_kommunekart_uten_hav_data WHERE "+attribute+" ILIKE "+inputString;
+   var query = "SELECT * FROM n2000_m_datestamps WHERE "+attribute+" ILIKE "+inputString;
    sublayers[0].setSQL(query);
    sublayers[0].setCartoCSS(cssyes);
   }
