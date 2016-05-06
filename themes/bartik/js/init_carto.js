4245f0445115fb0b4bbb89001a7d7e9aa62d11f5
Andreas Røed <andreas@kommunal-rapport.no>
2016-05-06 09:35:19 +0200

    Fixed date formats slider. Refactored out DataHandler and SliderHandler

--- themes/bartik/js/init_carto.js
+++ themes/bartik/js/init_carto.js
@@ -13,70 +13,30 @@ $(document).ready(function(){
       //Creates a new chart 
       ChartHandler.initChart();
 
-
-
-      function changeLegend(start, end) {
-          $('#legend').html("" + new Date(start).getDay()+"/"+new Date(start).getMonth()+"/"+new Date(start).getFullYear()+ " - "+new Date(end).getDay()+"/"+new Date(end).getMonth()+"/"+new Date(end).getFullYear());
-      }
-      function addTimeSlider(sublayer) {
-         var sql = cartodb.SQL({ user: 'andreasroeed' });
-         // fetch time range
-         sql.execute('SELECT max(vedtatt_dato), min(vedtatt_dato) FROM n2000_m_datestamps', function(data) {
-           var range = data.rows[0];
-           var max = new Date(range.max).getTime();
-           var min = new Date(range.min).getTime();
-
-           // update slider with range
-           $("#slider").slider({
-              min: min,
-              max: max,
-              values: [min,max],
-              slide: function(event, ui) {
-                // give feedback to the user on slide change
-                changeLegend(ui.values[0], ui.values[1]);
-              },
-              stop: function( event, ui ) { 
-                // when user selects the dates, update the layer with the range
-                // var start = new Date(ui.values[0]).toISOString()
-                // var end = new Date(ui.values[1]).toISOString();
-
-                startDate = new Date(ui.values[0]);
-                endDate = new Date(ui.values[1]);
-
-                start = startDate.getDay()+"/"+startDate.getMonth()+"/"+startDate.getFullYear()+"";
-                end = endDate.getDay()+"/"+endDate.getMonth()+"/"+endDate.getFullYear()+"";
-
-                var query = "SELECT * FROM n2000_m_datestamps WHERE vedtatt_dato >="+"'"+ start +"'"+" AND vedtatt_dato <="+"'"+ end +"'";
-                sublayers[0].setSQL(query);
-                sublayers[0].setCartoCSS(cssyes);
-              }
-            });
-            changeLegend(min, (min + max)/ 2);
-         });
-      }
       // Choose center and zoom level
-      var options = {
+    var options = {
         center: [63.44,10.42], // Trondheim
         zoom: 4,
         minZoom: 4,
         maxZoom: 6
-      }
+    }
 
-  // Instantiate map on specified DOM element
-  map = new L.Map('map',options);
-  var standard = 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
-  var stamen = 'http://tile.stamen.com/toner/{z}/{x}/{y}.png'
-  // Add a basemap to the map object just created
-  L.tileLayer(standard, {
+    // Instantiate map on specified DOM element
+    map = new L.Map('map',options);
+    var standard = 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
+    var stamen = 'http://tile.stamen.com/toner/{z}/{x}/{y}.png'
+    // Add a basemap to the map object just created
+    L.tileLayer(standard, {
     attribution: 'Stamen'
-  }).addTo(map);
+    }).addTo(map);
 
   cartodb.createLayer(map,layerUrl)
   .addTo(map)
   .on("done",function(layer){
       var sublayer = layer.getSubLayer(0);
       sublayers.push(sublayer);
-      addTimeSlider(sublayer[0]);
+
+      SliderHandler.addTimeSlider(sublayer[0]);
   })
   .on("error",function(err){
           alert("err: "+err);
@@ -86,21 +46,7 @@ $(document).ready(function(){
 
 window.onload = main;
 
-function getData(queryString,callback){
-  $(document).ready(function(){
-    var data; 
-    var sql = new cartodb.SQL({ user: 'andreasroeed' });
-    sql.execute(queryString)
-      .done(function(data) {
 
-        callback(data.rows[0]);
-    })
-      .error(function(errors) {
-      // errors contains a list of errors
-      console.log("errors:" + errors);
-    })
-  }); 
-}
 
 
 var layerSelections = {
@@ -145,14 +91,15 @@ $(document).ready(function(){
     }else{
       query = "SELECT ja,nei,blank,navn,deltakelse FROM n2000_m_datestamps WHERE navn ILIKE '"+$(this).val()+"'";
     }
-    getData(query, function(data){
-            console.log(data);
+
+    DataHandler.getData("andreasroeed",query, function(data){
             dataArray = [];
-            dataArray.push(data.ja);
-            dataArray.push(data.nei);
-            dataArray.push(data.blank);
-            dataArray.push(data.navn);
-            dataArray.push(data.deltakelse);
+            dataArray.push(data.rows[0].ja);
+            dataArray.push(data.rows[0].nei);
+            dataArray.push(data.rows[0].blank);
+            dataArray.push(data.rows[0].navn);
+            dataArray.push(data.rows[0].deltakelse);
+
             ChartHandler.updateChart(dataArray);
     });
 
---
+++ themes/bartik/js/slider_handler.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+
+SliderHandler = (function(){
+
+    changeLegend = function(start, end) {
+    	   var startMonth = new Date(start).getMonth() + 1;
+    	   var endMonth = new Date(end).getMonth() + 1;
+          $('#legend').html("" + new Date(start).getDate()+"/"+startMonth+"/"+new Date(start).getFullYear()+ " - "+new Date(end).getDate()+"/"+endMonth+"/"+new Date(end).getFullYear());
+    }
+
+    addTimeSlider = function(sublayer) {
+         var sql = cartodb.SQL({ user: 'andreasroeed' });
+         // fetch time range
+         DataHandler.getData('andreasroeed','SELECT max(vedtatt_dato), min(vedtatt_dato) FROM n2000_m_datestamps', function(data) {
+           var range = data.rows[0];
+           var max = new Date(range.max).getTime();
+           var min = new Date(range.min).getTime();
+
+
+           // update slider with range
+           $("#slider").slider({
+              min: min,
+              max: max,
+              values: [min,max],
+              slide: function(event, ui) {
+                // give feedback to the user on slide change
+                changeLegend(ui.values[0], ui.values[1]);
+              },
+              stop: function( event, ui ) { 
+              	var startDate = new Date(ui.values[0]);
+              	var endDate = new Date(ui.values[1]);
+              	var startMonth = startDate.getMonth() + 1;
+              	var endMonth = endDate.getMonth() + 1;
+
+              	var start = startMonth+"/"+startDate.getDate()+"/"+startDate.getFullYear();
+              	var end = endMonth+"/"+endDate.getDate()+"/"+endDate.getFullYear();
+
+                var query = "SELECT * FROM n2000_m_datestamps WHERE vedtatt_dato >="+"'"+ start +"'"+" AND vedtatt_dato <="+"'"+ end +"'";
+                console.log(query);
+                sublayers[0].setSQL(query);
+                sublayers[0].setCartoCSS(cssyes);
+              }
+            });
+            changeLegend(min, max);
+         });
+    }
+
+    return {
+    	addTimeSlider: addTimeSlider,
+    	changeLegend : changeLegend
+    }
+
+
+
+   })();
+
+
+});
