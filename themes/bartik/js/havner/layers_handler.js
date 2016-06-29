e9072fa68896c6cbc345723c32d1f7f357973d4b
Andreas Røed <andreas@kommunal-rapport.no>
2016-06-29 14:01:52 +0200

    Added havnekart to drupal. Made with cartoDB.js

---
+++ themes/bartik/js/havner/layers_handler.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  /**
+   *
+   * @return public object literal containing function objects
+   */
+  LayersHandler = (function() {
+
+    /**
+     * @param  {string} css - styling
+     * @param  {string} bigger - The value to compare with smaller
+     * @param  {string} smaller - The value to compare with bigger
+     * @return {undefined}
+     */
+    function showResults(css) {
+
+      sublayers[0].setSQL("SELECT * FROM oversikt_havner_data WHERE driftsinntekter_i_mill_kr IS NOT NULL");
+      sublayers[0].setCartoCSS(css);
+
+      var query = "SELECT * FROM oversikt_havner_data WHERE driftsinntekter_i_mill_kr IS NOT NULL";
+      // DataHandler.getData("andreasroeed",query, function(data) {
+      //
+      // });
+    };
+
+    return {
+      showResults: showResults
+    }
+
+  })();
+
+});
